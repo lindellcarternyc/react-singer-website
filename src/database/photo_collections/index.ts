e76f35bcb92df7841f01fa26943d5791a12eb2da
Lindell Carter <lindellcarternyc@gmail.com>
2018-03-14 22:55:51 -0400

    temporarily removed dummy collections

--- src/database/photo_collections/index.ts
+++ src/database/photo_collections/index.ts
@@ -1,37 +1,37 @@
 import { PhotoCollection } from '../../constants'
 
-const placeholder = require('../../assets/placeholder.png')
-const lindell =     require('../../assets/lindell_picture.jpg')
-const dialogues =   require('../../assets/dialogues1.jpg')
+// const placeholder = require('../../assets/placeholder.png')
+// const lindell =     require('../../assets/lindell_picture.jpg')
+// const dialogues =   require('../../assets/dialogues1.jpg')
 
 import MadamaButterflyPhotoCollection from './madama_butterfly'
 
 const PhotoCollections: { [id: string]: PhotoCollection } = {
   'madama_butterfly': MadamaButterflyPhotoCollection,
-  '0': {
-    id: '0',
-    name: 'Collection 1',
-    description: 'Description 1',
-    photos: [dialogues]
-  },
-  '1': {
-    id: '1',
-    name: 'Collection 2',
-    description: 'A second collection',
-    photos: []
-  },
-  '2': {
-    id: '2',
-    name: 'Col 3',
-    description: 'This is another collection',
-    photos: [lindell, placeholder]
-  },
-  '3': {
-    id: '3',
-    name: 'Collection #4',
-    description: 'A description is nice',
-    photos: [placeholder, placeholder, placeholder, placeholder]
-  }
+  // '0': {
+  //   id: '0',
+  //   name: 'Collection 1',
+  //   description: 'Description 1',
+  //   photos: [dialogues]
+  // },
+  // '1': {
+  //   id: '1',
+  //   name: 'Collection 2',
+  //   description: 'A second collection',
+  //   photos: []
+  // },
+  // '2': {
+  //   id: '2',
+  //   name: 'Col 3',
+  //   description: 'This is another collection',
+  //   photos: [lindell, placeholder]
+  // },
+  // '3': {
+  //   id: '3',
+  //   name: 'Collection #4',
+  //   description: 'A description is nice',
+  //   photos: [placeholder, placeholder, placeholder, placeholder]
+  // }
 }
 
 const getPhotoCollections = (): PhotoCollection[] => {
