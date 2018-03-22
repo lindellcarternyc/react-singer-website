0ef9cab554d8b7e5c26118fe75831fa79c3aa646
Lindell Carter <lindellcarternyc@gmail.com>
2018-03-21 21:44:26 -0400

    simplified folder structure

--- src/components/desktop-app-shell.tsx
+++ src/components/desktop-app-shell.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react'
 
-import SiteHeader from '../site-header'
-import Navigation from '../navigation'
-import Main from '../main'
+import SiteHeader from './site-header'
+import Navigation from './navigation'
+import Main from './main'
 
 export const DesktopAppShell = (): JSX.Element => {
   return (
